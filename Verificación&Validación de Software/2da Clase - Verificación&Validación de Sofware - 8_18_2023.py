a2ce0e3481b682ec868f7afd662b9895ed2f6949
teodawn <teodawn.td@gmail.com>
2023-08-18 16:58:31 -0600

    changes

---
+++ Verificación&Validación de Software/2da Clase - Verificación&Validación de Sofware - 8_18_2023.py
@@ -0,0 +1,28 @@
+la prueba fulanita cómo la relacionan con un documento ¿Sus documentos de prueba hacen esa 
+relación?
+
+Si tienes un buen desarrollo entonces tienes que tener trazabilidad entre los requerimientos y las pruebas
+
+"¿Estamos construyendo el software correctamente?"
+
+Si_agarramos_el_producto instalado
+
+¿Qué contesta el proceso de validación?
+
+
+Describime el prototipo de una casa ¿Qué vas a hacer con el plano? 
+El usuario tiene que tener un relación comercial con el porque sino metes al usuario
+te vas a equivocar en algun punto porque tu estarás interpretando las cosas y no él.
+
+
+Estamos intentando ganratizar que el trabajo a armadndose correctamente y tengo que 
+garantizar que lo que esté cotruyenodo esa adecuado.
+
+Describe muchos de los terminos y conceptos con los que lideams
+IEEE 24765
+
+Aquí está el 
+
+No porque microsoft no tenga fallas no significa que no tenga la calidad deseada.
+Se busca que se tenga el nivel adecuado de defectos. Teine que garantizar alcanzar esacalidad 
+y no es libre de defectos. por tanto es muy dificil que los usuarios accedan a pagarlo.
